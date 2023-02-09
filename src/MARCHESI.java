b11fcfb81946fb437cf4cae01f00ad3f9b13c6e6
utente <utente@DESKTOP-5GU5A7E>
2023-02-09 10:35:58 +0100

    modifica 1

--- src/MARCHESI.java
+++ src/MARCHESI.java
@@ -35,26 +35,51 @@ public class MARCHESI
 		System.out.println("2 - Es n. * - Titolo es. *");
 	}
 	
-	static void swap(int[]array, int i, int j)
-	{
-		int temp=array[j];
-		array[j]=array[i];
-		array[i]= temp;
-	}
 	static void es1()
 	{
 	int[]v= new int[5];
+	int[]v1= new int[5];
+	int[] v2= new int[v.length+v1.length];
 	for(int i=0;i<v.length;i++)
 		{
 		System.out.println("Inserisci numero");
 		v[i]=Integer.parseInt(in.nextLine());
 		}
-	int[]v1= new int[5];
+	
 	for(int j=0;j<v1.length;j++)
 		{
 		System.out.println("Inserisci numero");
 		v[j]=Integer.parseInt(in.nextLine());
 		}
+	for(int i=0;i<v.length;i++)
+	{
+		v2[i]=v[i];
+	}
+	for(int j=0;j<v1.length;j++)
+	{
+		v2[j+v.length]=v[j];
+	}
+	for(int i=0;i<v2.length;i++)
+	{
+		for(int j=0;j<v2.length;j++)
+		
+			if(v2[i]>v2[i+1])
+		{
+		swap(v2,i+1,i);
+		}
+		for(int z=0;z<v2.length;z++)
+		{
+			System.out.println(v2[z]);
+		}
+	}
+		
+	
 	 
+	}
+	static void swap(int[]array, int i, int j)
+	{
+		int temp=array[j];
+		array[j]=array[i];
+		array[i]= temp;
 	}
 }
